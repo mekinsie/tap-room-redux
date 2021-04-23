e3529d37e2b07c776d9db1c0443026c7c14edeed
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-23 11:44:00 -0700

    Add proptypes to keglist

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Keg from "./Keg";
+import PropTypes from "prop-types";
 
 function KegList(props){
   const styledKegList = {
@@ -25,5 +26,9 @@ function KegList(props){
     </React.Fragment>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+};
 
 export default KegList;
