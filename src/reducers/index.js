71ff18e11fc9b5abacf036c95f59df9ae52695ab
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 12:56:13 -0700

    Import formVisibleReducer into rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import kegListReducer from './keg-list-reducer';
+import formVisibleReducer from './form-visible-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  // formVisibleOnPage: formVisibleReducer,
+  formVisible: formVisibleReducer,
   masterKegList: kegListReducer
 });
 
