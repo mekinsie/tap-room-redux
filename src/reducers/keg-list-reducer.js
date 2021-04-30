551ea05d812e100b8c5f9b683b1c25ab40aebbdf
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 11:17:07 -0700

    Write add keg switch case for reducer

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,20 @@
+import * as c from './../actions/ActionTypes';
+
 export default(state = {}, action) => {
-  return state;
+const {name, ingredient, processingTime, sellPrice, pintsLeft, id} = action;
+switch(action.type) {
+  case c.ADD_KEG:
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        ingredient: ingredient,
+        processingTime: processingTime,
+        sellPrice: sellPrice,
+        pintsLeft: pintsLeft,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 }
