c979567c8ada5c28f4bd53320d1772db23c34f65
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 10:51:29 -0700

    Create action creator for add keg

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import * as c from './ActionTypes'
+
+export const addKeg = (keg) => {
+  const {name, ingredient, processingTime, sellPrice, pintsLeft, id} = keg;
+  return {
+    type: c.ADD_KEG,
+    name: name,
+    ingredient: ingredient,
+    processingTime: processingTime,
+    sellPrice: sellPrice,
+    pintsLeft: pintsLeft,
+    id: id
+  }
+}
+
