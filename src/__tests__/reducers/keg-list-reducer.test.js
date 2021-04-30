9bdd561065a971b78bb2da8ba7059071b9475d2b
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 11:09:33 -0700

    Write test for add keg action in reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,35 @@
-import kegListReducer from '../../reducers/keg-list-reducer'
+import kegListReducer from '../../reducers/keg-list-reducer';
+import * as c from '../../actions/ActionTypes';
+import * as a from '../../actions/index.js'
+
+let action;
+const kegFormData = {
+  name: "Beer",
+  ingredient: "Wheat",
+  processingTime: "1 Day",
+  sellPrice: "200",
+  pintsLeft: 124,
+  id: 1
+}
 
 describe('kegListRecuer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
+
+test('Should add a keg to the master keg list', () => {
+  action = a.addKeg(kegFormData)
+  expect(kegListReducer({}, action)).toEqual({
+    [1] : {
+      name: "Beer",
+      ingredient: "Wheat",
+      processingTime: "1 Day",
+      sellPrice: "200",
+      pintsLeft: 124,
+      id: 1
+    }
+  });
+});
+
+
 });
