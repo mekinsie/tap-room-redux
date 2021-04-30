29a7b6163daf2a0aabe4779c7ae55dfaa243de40
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 16:31:17 -0700

    Write type case to sell pint of keg

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -5,6 +5,10 @@ export default (state = null, action ) => {
   switch(action.type){
   case c.SELECT_KEG:
     return keg;
+  case c.SELL_PINT:
+    let newKeg = {...keg}
+    newKeg.pintsLeft -= 1
+    return newKeg;
   default:
     return state;
   }
