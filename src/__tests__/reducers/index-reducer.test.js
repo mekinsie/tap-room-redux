c7d7dfdeca4861c0e96b0f9e014dc0c6f28fd301
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 12:59:30 -0700

    Test initial state of root reducer kegList

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,10 +1,21 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe("rootReducer", () => {
+  let store = createStore(rootReducer);
+  
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, {type: null})).toEqual({
       masterKegList: {},
       formVisible: false
     });
   });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+
 });
