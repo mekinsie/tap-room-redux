e1de85dec6f3d0c3708db299c9958f83143caa5a
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 11:58:15 -0700

    Subscribe to store

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer);
 
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
 ReactDOM.render(
   <Provider store= {store}>
     <App />
