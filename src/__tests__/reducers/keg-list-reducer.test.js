8c7b4a281cc053de6a23546b8e657e15f730664e
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 11:24:26 -0700

    Write test for delete keg action

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,34 +2,66 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 import * as c from '../../actions/ActionTypes';
 import * as a from '../../actions/index.js'
 
-let action;
-const kegFormData = {
-  name: "Beer",
-  ingredient: "Wheat",
-  processingTime: "1 Day",
-  sellPrice: "200",
-  pintsLeft: 124,
-  id: 1
-}
 
 describe('kegListRecuer', () => {
-  test('Should return default state if no action type is recognized', () => {
-    expect(kegListReducer({}, {type: null})).toEqual({});
-  });
+  let action;
+  const kegFormData = {
+    name: "Beer",
+    ingredient: "Wheat",
+    processingTime: "1 Day",
+    sellPrice: "200",
+    pintsLeft: 124,
+    id: 1
+  }
 
-test('Should add a keg to the master keg list', () => {
-  action = a.addKeg(kegFormData)
-  expect(kegListReducer({}, action)).toEqual({
-    [1] : {
+  const currentState = {
+    1: {
       name: "Beer",
       ingredient: "Wheat",
       processingTime: "1 Day",
       sellPrice: "200",
       pintsLeft: 124,
       id: 1
+    },
+    2: {
+      name: "Pale Ale",
+      ingredient: "Hops",
+      processingTime: "1-2 Days",
+      sellPrice: "300",
+      pintsLeft: 124,
+      id: 2
     }
+  }
+  test('Should return default state if no action type is recognized', () => {
+    expect(kegListReducer({}, {type: null})).toEqual({});
   });
-});
 
+  test('Should add a keg to the master keg list', () => {
+    action = a.addKeg(kegFormData)
+    expect(kegListReducer({}, action)).toEqual({
+      [1] : {
+        name: "Beer",
+        ingredient: "Wheat",
+        processingTime: "1 Day",
+        sellPrice: "200",
+        pintsLeft: 124,
+        id: 1
+      }
+    });
+  });
+
+  test('Should delete a keg from the master keg list', () => {
+    action = a.deleteKeg(1)
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {
+        name: "Pale Ale",
+        ingredient: "Hops",
+        processingTime: "1-2 Days",
+        sellPrice: "300",
+        pintsLeft: 124,
+        id: 2
+      }
+    });
+  });
 
 });
