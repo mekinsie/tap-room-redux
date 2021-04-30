87c0a5bcc441802c99fb244d31c03d62921a98fa
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-30 11:44:44 -0700

    Create store with access to root reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import rootReducer from './reducers/index.js';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
